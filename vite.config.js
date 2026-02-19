77c8e7d9a6d6a864cabe98699da3479e68032681
perfecttraders <chandwanirohit94@gmail.com>
2026-02-19 19:00:42 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-    base: env.VITE_BASE_PATH || '/',
-    server: {
-      port: 5173,
-      host: '0.0.0.0'
-    },
-    build: {
-      sourcemap: false,
-      chunkSizeWarningLimit: 1200
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/website/', // VERY IMPORTANT
 })
+
